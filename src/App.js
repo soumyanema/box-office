1c6d54c513b19c78304778e4989e408181da58a8
soumya <soumyanema03@gmail.com>
2021-12-21 14:55:13 +0530

    added layout management

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Navs from "./components/Navs.jsx";
 import Home from "./pages/Home.jsx";
 import Starred from "./pages/Starred.jsx";
 
 function App() {
   return (
     <div>
-      <Navs />
       <Switch>
         <Route exact path="/">
           <Home />
