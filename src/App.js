a2c08229706a57a6c13aebc3c2866d88f14121fa
soumya <soumyanema03@gmail.com>
2021-12-24 17:11:35 +0530

    Created show page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import Show from "./pages/Show.jsx";
 import Starred from "./pages/Starred.jsx";
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
         </Route>
         <Route exact path="/starred">
           <Starred />
+        </Route>
+        <Route exact path="/show/:id">
+          <Show />
         </Route>
         <Route>
           <div>Page not found</div>
